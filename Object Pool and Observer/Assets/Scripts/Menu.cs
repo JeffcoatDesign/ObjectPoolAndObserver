35c98a8fbecfbc133ef7552e5fb94829708d1b61
JeffcoatDesign <98057241+JeffcoatDesign@users.noreply.github.com>
2023-09-20 14:16:04 -0700

    WebGL

---
+++ Object Pool and Observer/Assets/Scripts/Menu.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Menu : MonoBehaviour
+{
+    public void ChapterEight()
+    {
+        SceneManager.LoadScene("Ch8");
+    }
+    public void ChapterNine()
+    {
+        SceneManager.LoadScene("Ch9");
+    }
+}
